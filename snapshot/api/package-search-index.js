ec79d10978ac674ae2a5df8ace894303f4060163
sdelamo <sdelamo@users.noreply.github.com>
2026-05-13 12:31:28 +0000

    Deploying to gh-pages - 12:31:27

--- snapshot/api/package-search-index.js
+++ snapshot/api/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"io.micronaut.configuration.picocli"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"io.micronaut.configuration.picocli"},{"l":"io.micronaut.picocli.info"}];updateSearchResults();
